f716ba31f159e0b15f5b5a87da5cc7b4083812bf
Felipe Luis Korbes <91323659+luis-fk@users.noreply.github.com>
2024-02-13 19:48:38 -0300

    Delete deals-tracker directory

--- deals-tracker/src/components/ListDivider/index.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { Container } from './styles';
-
-/* Simple list divider to be used in a FlatList */
-export function ListDivider(){
-    return(
-        <Container />
-    );
-}
